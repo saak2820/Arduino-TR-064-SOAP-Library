96297d435dff1558ee1491816532144937443843
Karsten Sauer <35265177+saak2820@users.noreply.github.com>
2023-06-06 13:19:40 +0200

    Update renamesecrets.sh

--- scripts/renamesecrets.sh
+++ scripts/renamesecrets.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-for filename in examples/arduino_secrets.h.example; do
-    [ -e "$filename" ] || continue
-    cp $filename arduino_secrets.h
-done
+
+
+for pathname in examples/*/arduino_secrets.h.example; do
+	[ -e "$pathname" ] || continue
+	printf 'Processing "%s"\n' "$pathname" >&2
+	
+    #${var/Pattern/Replacement}
+    new_file=${pathname/arduino_secrets.h.example/arduino_secrets.h};
+	printf 'new_file "%s"\n' "$new_file" >&2
+    cp "$pathname" "${new_file}";
+done;
+
