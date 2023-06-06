ada3d06d53c374222048ba1701f1a1d2fbb395e3
Karsten Sauer <35265177+saak2820@users.noreply.github.com>
2023-06-06 11:12:32 +0200

    script

--- scripts/renamesecrets.sh
+++ scripts/renamesecrets.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-
-EXAMPLEFILE := arduino_secrets.h.example
-SUBDIRS := $(wildcard examples/*)
-
-all: $(SUBDIRS)
-$(SUBDIRS):
-	# If $(EXAMPLEFILE) exists, move it to arduino_secrets.h
-	@echo "Find " $@/$(EXAMPLEFILE)
-	@if [ -f $@/$(EXAMPLEFILE) ]; then \
-		mv $@/$(EXAMPLEFILE) $@/arduino_secrets.h; \
-	else echo "Not found"; \
-	fi
-
-.PHONY: all $(SUBDIRS)
+for filename in Data/*.txt; do
+    [ -e "$filename" ] || continue
+    cp $filename arduino_secrets.h
+done
